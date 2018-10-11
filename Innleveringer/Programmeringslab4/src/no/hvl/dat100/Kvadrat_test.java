9f3d89d7fb84641f8521ff5de29e809745e70144
h134029 <134029@stud.hvl.no>
2018-10-11 22:41:22 +0200

    proglab4

---
+++ Innleveringer/Programmeringslab4/src/no/hvl/dat100/Kvadrat_test.java
@@ -0,0 +1,28 @@
+package no.hvl.dat100;
+
+public class Kvadrat_test {
+    public static void main(String[] args) {
+
+        Kvadrat kv3 = new Kvadrat(3);
+        Kvadrat kv4 = new Kvadrat(4);
+        Kvadrat kv5 = new Kvadrat(5);
+
+        // Kv 3
+        kv3.PrintInfo();
+        kv3.Tegne();
+        System.out.println();
+
+        // Kv 4
+        kv4.PrintInfo();
+        kv4.Tegne();
+        System.out.println();
+
+        // Kv 5
+        kv5.PrintInfo();
+        kv5.Tegne();
+        System.out.println();
+
+        // Sum
+        System.out.println(kv5.sum(kv3, kv4));
+    }
+}
---
+++ Innleveringer/Programmeringslab4/src/no/hvl/dat100/Main.java
@@ -0,0 +1,11 @@
+package no.hvl.dat100;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Kvadrat k = new Kvadrat(4);
+        k.Tegne();
+        k.Diagonal();
+
+    }
+}
