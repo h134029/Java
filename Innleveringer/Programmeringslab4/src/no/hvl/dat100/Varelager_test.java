1763174a29007d91cf8bb0862cc0d98cee640647
h134029 <134029@stud.hvl.no>
2018-10-22 18:19:59 +0200

    proglab4

---
+++ Innleveringer/Programmeringslab4/src/no/hvl/dat100/Varelager_test.java
@@ -0,0 +1,27 @@
+package no.hvl.dat100;
+
+public class Varelager_test {
+    public static void main(String[] args) {
+        Vare v1 = new Vare(555,"Sjokolade",13.50);
+        Vare v2 = new Vare(666,"Ananas",66.66);
+        Vare v3 = new Vare(777, "Cola",19.99);
+        Varelager VL1 = new Varelager(10);
+
+        VL1.leggTil(v1);
+        VL1.leggTil(v2);
+        VL1.leggTil(v3);
+        VL1.leggTil(v2);
+        VL1.leggTil(v1);
+        VL1.leggTil(v2);
+        VL1.leggTil(v3);
+        VL1.leggTil(v2);
+        VL1.leggTil(v1);
+        VL1.leggTil(v1);
+
+        VL1.slett(666);
+
+        for (int i=0; i < 10;i++) {
+            System.out.println(VL1.samling[i]);
+        }
+    }
+}
